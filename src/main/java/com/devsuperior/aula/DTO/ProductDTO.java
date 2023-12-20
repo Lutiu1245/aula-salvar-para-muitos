14f1cbd8c06958d14d581598369b5668a8b24b2f
Luan Henrique <99328979+Lutiu1245@users.noreply.github.com>
2023-12-20 07:44:27 -0300

    feat: add DTOS

---
+++ src/main/java/com/devsuperior/aula/DTO/ProductDTO.java
@@ -0,0 +1,42 @@
+package com.devsuperior.aula.DTO;
+
+import com.devsuperior.aula.entities.Category;
+import com.devsuperior.aula.entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductDTO {
+    private Long id;
+    private String name;
+    private Double price;
+
+    private List<CategoryDTO> categories = new ArrayList<>();
+
+    public ProductDTO(Long id, String name, Double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public ProductDTO(Product product) {
+        id = product.getId();
+        name = product.getName();
+        price = product.getPrice();
+        for (Category categoryDTO : product.getCategories()) {
+            categories.add(new CategoryDTO(categoryDTO));
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+}
